047d413c9102e11f0a3d42b68657df4bfe009ae4
Arash-kaffashian <kaffashian.arash@gmail.com>
2026-02-24 19:39:24 +0330

    code reworked

--- services/post_service/app/services/post_service.py
+++ services/post_service/app/services/post_service.py
@@ -9,11 +9,8 @@ from ..events.publisher import publish_post_deleted
 
 # bridge between router and crud to publish post_deleted for other services
 async def delete_post(db, post_id: int):
-    post = crud.delete_post(db, post_id)
-
-    if not post:
-        raise HTTPException(status_code=404, detail="Post not found")
+    crud.delete_post(db, post_id)
 
     await publish_post_deleted(post_id)
 
-    return {"detail": "Post deleted successfully"}
+    return
